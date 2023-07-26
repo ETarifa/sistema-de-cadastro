1856eb9f5dff0c35f438670e05474b1a32e5c063
Elverson Vilharroel Tarifa <6203@schulze.com.br>
2023-07-26 17:09:34 -0300

    Voltando para o estado anterior

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-//mais uma inserção de teste
-console.log("Enviando para o GitHub")
+
